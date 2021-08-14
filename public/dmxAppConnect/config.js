aa0820cdf2fbe7adc60ef725206525b5f9666758
Harshit <swi.private@gmail.com>
2021-08-14 17:41:17 +0530

    --users CRUDE properties implemented

---
+++ public/dmxAppConnect/config.js
@@ -0,0 +1,182 @@
+dmx.config({
+  "users": {
+    "dvUsers": {
+      "meta": [
+        {
+          "name": "id",
+          "type": "number"
+        },
+        {
+          "name": "name",
+          "type": "text"
+        },
+        {
+          "name": "email",
+          "type": "text"
+        },
+        {
+          "name": "mobile",
+          "type": "text"
+        },
+        {
+          "name": "password",
+          "type": "text"
+        },
+        {
+          "name": "saltvalue",
+          "type": "text"
+        },
+        {
+          "name": "status",
+          "type": "text"
+        },
+        {
+          "name": "user_type",
+          "type": "text"
+        },
+        {
+          "name": "super_user",
+          "type": "text"
+        },
+        {
+          "name": "remove",
+          "type": "text"
+        },
+        {
+          "name": "blank1",
+          "type": "text"
+        },
+        {
+          "name": "blank2",
+          "type": "text"
+        },
+        {
+          "name": "blank3",
+          "type": "text"
+        },
+        {
+          "name": "blank4",
+          "type": "text"
+        },
+        {
+          "name": "image",
+          "type": "text"
+        },
+        {
+          "name": "created_by",
+          "type": "text"
+        },
+        {
+          "name": "created_on",
+          "type": "datetime"
+        },
+        {
+          "name": "updated_by",
+          "type": "text"
+        },
+        {
+          "name": "updated_on",
+          "type": "datetime"
+        },
+        {
+          "name": "deleted_by",
+          "type": "text"
+        },
+        {
+          "name": "deleted_on",
+          "type": "datetime"
+        }
+      ],
+      "outputType": "array"
+    },
+    "ddUsers": {
+      "meta": [
+        {
+          "name": "id",
+          "type": "number"
+        },
+        {
+          "name": "name",
+          "type": "text"
+        },
+        {
+          "name": "email",
+          "type": "text"
+        },
+        {
+          "name": "mobile",
+          "type": "text"
+        },
+        {
+          "name": "password",
+          "type": "text"
+        },
+        {
+          "name": "saltvalue",
+          "type": "text"
+        },
+        {
+          "name": "status",
+          "type": "text"
+        },
+        {
+          "name": "user_type",
+          "type": "text"
+        },
+        {
+          "name": "super_user",
+          "type": "text"
+        },
+        {
+          "name": "remove",
+          "type": "text"
+        },
+        {
+          "name": "blank1",
+          "type": "text"
+        },
+        {
+          "name": "blank2",
+          "type": "text"
+        },
+        {
+          "name": "blank3",
+          "type": "text"
+        },
+        {
+          "name": "blank4",
+          "type": "text"
+        },
+        {
+          "name": "image",
+          "type": "text"
+        },
+        {
+          "name": "created_by",
+          "type": "text"
+        },
+        {
+          "name": "created_on",
+          "type": "datetime"
+        },
+        {
+          "name": "updated_by",
+          "type": "text"
+        },
+        {
+          "name": "updated_on",
+          "type": "datetime"
+        },
+        {
+          "name": "deleted_by",
+          "type": "text"
+        },
+        {
+          "name": "deleted_on",
+          "type": "datetime"
+        }
+      ],
+      "outputType": "array"
+    }
+  }
+});
