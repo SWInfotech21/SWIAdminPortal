8887e95457e1cceb2ed510c0257e58185af3a0d9
DESKTOP-34ARMFG\shivalik <swi.private@gmail.com>
2021-08-30 21:44:57 +0530

    --30th aug submit

--- extensions/server_connect/formatters/custom.js
+++ extensions/server_connect/formatters/custom.js
@@ -1,14 +1,14 @@
-const diacritics = require('../../../lib/core/diacritics');
-import fs from "fs"; 
-import { parseString } from "xml2js"; 
+// const diacritics = require('../../../lib/core/diacritics');
+// import fs from "fs"; 
+// import { parseString } from "xml2js"; 
 
-exports.xmlToJSON = function (arr) {
-    parseString(xmldata, function (err, arr) {
-  
-	// parsing to json
-	let data = JSON.stringify(arr)
-	  
-	// display the json data
-	return data;
-	});
-}
+// exports.xmlToJSON = function (arr) {
+//     parseString(xmldata, function (err, arr) {
+
+// 	// parsing to json
+// 	let data = JSON.stringify(arr)
+
+// 	// display the json data
+// 	return data;
+// 	});
+// }
