d6b17d3e27094f9a52940fffa273ac50d76b5f4c
DESKTOP-34ARMFG\shivalik <harshitmaheshwari265@gmail.com>
2021-12-15 14:06:53 +0530

    --submit xmltojs

--- extensions/server_connect/formatters/custom.js
+++ extensions/server_connect/formatters/custom.js
@@ -7,23 +7,23 @@ var parser = new xml2js.Parser(/* options */);
 exports.xmlToJSON = function (xmldata) {
 
 
-	var xml = "<root>Hello xml2js!</root>"
-	// return parseString(xmldata, function (err, result) {
-	// 		return	JSON.stringify(result)
-	// });
+    var xml = "<root>Hello xml2js!</root>"
+    // return parseString(xmldata, function (err, result) {
+    // 		return	JSON.stringify(result)
+    // });
 
-	return parser.parseStringPromise(xmldata).then(function (result) {
-		return	result
-	  })
-	  .catch(function (err) {
-		// Failed
-	  });
-	// parseString(xmldata, function (err, arr) {
+    return parser.parseStringPromise(xmldata).then(function (result) {
+        return result
+    })
+        .catch(function (err) {
+            // Failed
+        });
+    // parseString(xmldata, function (err, arr) {
 
-	// // parsing to json
-	// let data = JSON.stringify(arr)
+    // // parsing to json
+    // let data = JSON.stringify(arr)
 
-	// // display the json data
-	// return data;
-	// });
+    // // display the json data
+    // return data;
+    // });
 }
